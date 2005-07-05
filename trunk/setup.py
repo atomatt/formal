22880bfb68f3490837ca4b8b86ac390aa5f0af3b
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2005-07-05 17:00:38 +0000

    Add an egg-ified setup

---
+++ trunk/setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+import forms
+
+setup(
+    name='forms',
+    version=forms.version,
+    description='HTML forms framework for Nevow',
+    author='Matt Goodall',
+    author_email='matt@pollenation.net',
+    packages=['forms', 'forms.test'],
+    )
