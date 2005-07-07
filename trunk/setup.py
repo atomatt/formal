2baa52ae31415e66c54f1e29b685f6382f585606
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2005-07-07 21:13:00 +0000

    Use py2.4' package_data thing instead of hacking it.

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,12 +2,8 @@ try:
     from setuptools import setup
 except:
     from distutils.core import setup
-from distutils.command import install
 
 import forms
-
-for scheme in install.INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
 
 setup(
     name='forms',
@@ -16,7 +12,7 @@ setup(
     author='Matt Goodall',
     author_email='matt@pollenation.net',
     packages=['forms', 'forms.test'],
-    data_files=[
-        ['forms', ['forms/forms.css']],
-        ]
+    package_data={
+        'forms': ['forms.css'],
+        }
     )
