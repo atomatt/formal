c4f2946f67784c24c2364821a2ba93773ac96e88
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2005-07-07 14:08:03 +0000

    Include forms.css in the package

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ try:
     from setuptools import setup
 except:
     from distutils.core import setup
+from distutils.command import install
 
 import forms
+
+for scheme in install.INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
 setup(
     name='forms',
@@ -12,4 +16,7 @@ setup(
     author='Matt Goodall',
     author_email='matt@pollenation.net',
     packages=['forms', 'forms.test'],
+    data_files=[
+        ['forms', ['forms/forms.css']],
+        ]
     )
