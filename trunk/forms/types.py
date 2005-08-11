0438ac2ab69a7e91c6957587fc98e5d131d2c115
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2005-08-11 22:25:08 +0000

    Extract Type base class.
    There was too much in common between all the different Type classes so I
    extracted a base class. It's makes it a teeny bit more complicated but it's
    surely better this way :-).
    
    The Type class is now entirely keyword arg driven; subclasses should probably
    consider doing this too.

--- trunk/forms/types.py
+++ trunk/forms/types.py
@@ -6,205 +6,89 @@ from forms import iforms, validation
 from zope.interface import implements
 
 
-class String(object):
-    
+class Type(object):
+
     implements( iforms.IType )
-    
+
+    # Name of the instance
     name = None
+    # Instance is required
     required = False
-    strip = False
+    # Value to use if no value entered
     missing = None
+    # Instance cannot be changed
     immutable = False
-    
-    def __init__(self, required=None, strip=None, missing=None, immutable=False):
-        if required is not None: 
+
+    def __init__(self, name=None, required=None, missing=None, immutable=None):
+        if name is not None:
+            self.name = name
+        if required is not None:
             self.required = required
-        if strip is not None:
-            self.strip = strip
         if missing is not None:
             self.missing = missing
-        self.immutable = immutable
+        if immutable is not None:
+            self.immutable = immutable
+
+    def validate(self, value):
+        if self.required and value is None:
+            raise validation.FieldRequiredError('required field')
+        if value is None:
+            value = self.missing
+        return value
+
+
+class String(Type):
+
+    # Strip the value before validation
+    strip = False
+    
+    def __init__(self, **k):
+        strip = k.pop('strip', None)
+        if strip is not None:
+            self.strip = strip
+        super(String, self).__init__(**k)
         
     def validate(self, value):
         if value is not None and self.strip:
             value = value.strip()
         if not value:
             value = None
-        if self.required and value is None:
-            raise validation.FieldRequiredError('required field')
-        if value is None:
-            value = self.missing
-        return value
+        return super(String, self).validate(value)
         
         
-class Integer(object):
-    
-    implements( iforms.IType )
-    
-    name = None
-    required = False
-    missing = None
-    immutable = False
-   
-    def __init__(self, required=None, missing=None, immutable=False):
-        if required is not None:
-            self.required = required
-        if missing is not None:
-            self.missing = missing
-        self.immutable = immutable
-        
-    def validate(self, value):
-        if self.required and value is None:
-            raise validation.FieldRequiredError('required field')
-        if value is None:
-            value = self.missing
-        return value
+class Integer(Type):
+    pass
         
         
-class Float(object):
-    
-    implements( iforms.IType )
-    
-    name = None
-    required = False
-    missing = None
-    immutable = False
-    
-    def __init__(self, required=None, missing=None, immutable=False):
-        if required is not None:
-            self.required = required
-        if missing is not None:
-            self.missing = missing
-        self.immutable = immutable
-        
-    def validate(self, value):
-        if self.required and value is None:
-            raise validation.FieldRequiredError('required field')
-        if value is None:
-            value = self.missing
-        return value
+class Float(Type):
+    pass
         
         
-class Boolean(object):
-    
-    implements( iforms.IType )
-    
-    name = None
-    required = False
-    missing = None
-    immutable = False
-    
-    def __init__(self, required=None, missing=None, immutable=False):
-        if required is not None:
-            self.required = required
-        if missing is not None:
-            self.missing = missing
-        self.immutable = immutable
+class Boolean(Type):
+    pass
+        
+        
+class Date(Type):
+    pass
+        
+        
+class Time(Type):
+    pass
+        
+        
+class Sequence(Type):
 
-    def validate(self, value):
-        if self.required and value is None:
-            raise validation.FieldRequiredError('required field')
-        if value is None:
-            value = self.missing
-        return value
-        
-        
-class Date(object):
-    
-    implements( iforms.IType )
-    
-    name = None
-    required = None
-    missing = None
-    immutable = False
-    
-    def __init__(self, required=None, missing=None, immutable=False):
-        if required is not None:
-            self.required = required
-        if missing is not None:
-            self.missing = missing
-        self.immutable = immutable
-        
-    def validate(self, value):
-        if self.required and value is None:
-            raise validation.FieldRequiredError('required field')
-        if value is None:
-            value = self.missing
-        return value
-        
-        
-class Time(object):
-    
-    implements( iforms.IType )
-    
-    name = None
-    required = None 
-    missing = None
-    immutable = False
-        
-    def __init__(self, required=None, missing=None, immutable=False):
-        if required is not None:
-            self.required = required
-        if missing is not None:
-            self.missing = missing
-        self.immutable = immutable
-            
-    def validate(self, value):
-        if self.required and value is None:
-            raise validation.FieldRequiredError('required field')
-        if value is None:
-            value = self.missing
-        return value
-        
-        
-class Sequence(object):
-    implements( iforms.IType )
-    
-    name = None
-    required = None 
-    missing = None
+    # Type of items in the sequence
     type = None
-    immutable = False
 
-    def __init__(self, type=None, required=None, missing=None, immutable=False):
-        if required is not None:
-            self.required = required
-        if missing is not None:
-            self.missing = missing
+    def __init__(self, type=None, **k):
+        super(Sequence, self).__init__(**k)
         if type is not None:
             self.type = type
-        self.immutable = immutable
-            
-    def validate(self, value):
-        if self.required and not value:
-            raise validation.FieldRequiredError('needs at least one item checked')
-        if value is None:
-            value = self.missing
-        return value
 
 
-class File(object):
-    implements( iforms.IType )
-    
-    name = None
-    required = False
-    missing = None
-    immutable = False
-   
-    def __init__(self, required=None, missing=None, immutable=False):
-        if required is not None:
-            self.required = required
-        if missing is not None:
-            self.missing = missing
-        self.immutable = immutable
-        
-    def validate(self, value):
-        if self.required and value is None:
-            raise validation.FieldRequiredError('required field')
-        if value is None:
-            value = self.missing
-        return value
-
+class File(Type):
+    pass
 
 
 __all__ = [
