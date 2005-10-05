93292aa999df14ed4f4e210a7b421668a5cb8271
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2005-10-05 11:40:39 +0000

    Tag 0.2.4 release.

---
+++ 0.2.4/forms/examples/dates.py
@@ -0,0 +1,17 @@
+import forms
+
+title = 'Dates'
+description = 'Date entry examples'
+
+def makeForm(ctx):
+    form = forms.Form()
+    form.addField('isoFormat', forms.Date(), forms.TextInput)
+    form.addField('monthFirst', forms.Date(), forms.DatePartsInput)
+    form.addField('dayFirst', forms.Date(), forms.widgetFactory(forms.DatePartsInput, dayFirst=True))
+    form.addField('monthAndYear', forms.Date(), forms.MMYYDatePartsInput)
+    form.addAction(formSubmitted)
+    return form
+
+def formSubmitted(ctx, form, data):
+    print form, data
+
