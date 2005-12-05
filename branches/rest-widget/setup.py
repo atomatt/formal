90cbedad33ee1d6e49781e97a4529dce2dee9544
damian <damian@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2005-12-05 11:09:39 +0000

    Changed name of egg for this branch.

--- branches/rest-widget/setup.py
+++ branches/rest-widget/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 import forms
 
 setup(
-    name='forms',
+    name='forms-rest',
     version=forms.version,
-    description='HTML forms framework for Nevow',
+    description='HTML forms framework for Nevow, branch with ReST control',
     author='Matt Goodall',
     author_email='matt@pollenation.net',
     packages=find_packages(),
