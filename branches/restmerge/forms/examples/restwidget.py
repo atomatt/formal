7685d58a5d4e911a9bec7c55f6861c78f6713f46
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2005-12-21 14:03:54 +0000

    Can't the specific ImportError in the ReSTTextArea and update ReST example so that the examples can run without docutils installed.

--- branches/restmerge/forms/examples/restwidget.py
+++ branches/restmerge/forms/examples/restwidget.py
@@ -1,28 +1,41 @@
 import forms
 from forms.examples import main
-from docutils.writers.html4css1 import Writer, HTMLTranslator
-from docutils import nodes
+
+# Let the examples run if docutils is not installed
+try:
+    import docutils
+except ImportError:
+    import warnings
+    warnings.warn("docutils is not installed")
+    docutilsAvailable = False
+else:
+    docutilsAvailable = True
 
 
-class CustomisedHTMLTranslator(HTMLTranslator):
-    def visit_newdirective_node(self, node):
-        self.body.append('<div>Some HTML with a %s parameter</div>\n'%(node.attributes['parameter'],))
+if docutilsAvailable:
 
-    def depart_newdirective_node(self, node):
+    from docutils.writers.html4css1 import Writer, HTMLTranslator
+    from docutils import nodes
+
+    class CustomisedHTMLTranslator(HTMLTranslator):
+        def visit_newdirective_node(self, node):
+            self.body.append('<div>Some HTML with a %s parameter</div>\n'%(node.attributes['parameter'],))
+
+        def depart_newdirective_node(self, node):
+            pass
+
+    class newdirective_node(nodes.General, nodes.Inline, nodes.TextElement):
         pass
 
-class newdirective_node(nodes.General, nodes.Inline, nodes.TextElement):
-    pass
+    def newdirective(name, arguments, options, content, lineno,
+                     content_offset, block_text, state, state_machine):
+        return [newdirective_node('', parameter=arguments[0])]
 
-def newdirective(name, arguments, options, content, lineno,
-                 content_offset, block_text, state, state_machine):
-    return [newdirective_node('', parameter=arguments[0])]
+    newdirective.arguments = (1, 0, 0)
+    newdirective.options = None
 
-newdirective.arguments = (1, 0, 0)
-newdirective.options = None
-
-from docutils.parsers.rst import directives
-directives.register_directive('newdirective', newdirective)
+    from docutils.parsers.rst import directives
+    directives.register_directive('newdirective', newdirective)
 
 
 class ReSTWidgetFormPage(main.FormExamplePage):
@@ -31,13 +44,15 @@ class ReSTWidgetFormPage(main.FormExamplePage):
     description = 'The ReST widget captures ReST and previews as HTML.'
     
     def form_example(self, ctx):
-        w = Writer()
-        w.translator_class = CustomisedHTMLTranslator
-
         form = forms.Form()
-        form.addField('restString', forms.String(required=True), forms.widgetFactory(forms.ReSTTextArea, restWriter=w))
+        form.addField('restString', forms.String(required=True),
+                widgetFactory=forms.ReSTTextArea)
+        if docutilsAvailable:
+            w = Writer()
+            w.translator_class = CustomisedHTMLTranslator
+            form.addField('customRestString', forms.String(required=True),
+                    forms.widgetFactory(forms.ReSTTextArea, restWriter=w))
         form.addAction(self.submitted)
-
         return form
 
     def submitted(self, ctx, form, data):
