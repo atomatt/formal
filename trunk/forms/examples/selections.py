908073e0e41cc253a5679bdc685da3826508ce8e
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2006-03-14 10:33:36 +0000

    Wrap at 80 chars wide.

--- trunk/forms/examples/selections.py
+++ trunk/forms/examples/selections.py
@@ -31,17 +31,32 @@ class SelectionFormPage(main.FormExamplePage):
     def form_example(self, ctx):
         form = forms.Form()
         form.addField('required', forms.String(required=True))
-        form.addField('oneString', forms.String(), forms.widgetFactory(forms.SelectChoice, options=strings))
-        form.addField('anotherString', forms.String(), forms.widgetFactory(forms.SelectChoice, options=data_strings))
-        form.addField('oneMoreString', forms.String(required=True), forms.widgetFactory(forms.RadioChoice, options=data_strings))
-        form.addField('oneDate', forms.Date(), forms.widgetFactory(forms.SelectChoice, options=dates))
-        form.addField('multipleStrings', forms.Sequence(forms.String()), forms.widgetFactory(forms.CheckboxMultiChoice, options=strings))
-        form.addField('multipleDates', forms.Sequence(forms.Date()), forms.widgetFactory(forms.CheckboxMultiChoice, options=dates))
-        form.addField('differentNoneSelect', forms.String(), forms.widgetFactory(forms.SelectChoice, options=strings, noneOption=differentNone))
-        form.addField('differentNoneRadios', forms.String(), forms.widgetFactory(forms.RadioChoice, options=data_strings, noneOption=differentNone))
-        form.addField('selectOther', forms.String(), forms.widgetFactory(forms.SelectOtherChoice, options=['Mr', 'Mrs']))
-        form.addField('selectOtherRequired', forms.String(required=True), forms.widgetFactory(forms.SelectOtherChoice, options=['Mr', 'Mrs']))
-        form.addField('multiselect', forms.String(), forms.widgetFactory(forms.MultiselectChoice, options=strings))
+        form.addField('oneString', forms.String(),
+                forms.widgetFactory(forms.SelectChoice, options=strings))
+        form.addField('anotherString', forms.String(),
+                forms.widgetFactory(forms.SelectChoice, options=data_strings))
+        form.addField('oneMoreString', forms.String(required=True),
+                forms.widgetFactory(forms.RadioChoice, options=data_strings))
+        form.addField('oneDate', forms.Date(),
+                forms.widgetFactory(forms.SelectChoice, options=dates))
+        form.addField('multipleStrings', forms.Sequence(forms.String()),
+                forms.widgetFactory(forms.CheckboxMultiChoice, options=strings))
+        form.addField('multipleDates', forms.Sequence(forms.Date()),
+                forms.widgetFactory(forms.CheckboxMultiChoice, options=dates))
+        form.addField('differentNoneSelect', forms.String(),
+                forms.widgetFactory(forms.SelectChoice, options=strings,
+                    noneOption=differentNone))
+        form.addField('differentNoneRadios', forms.String(),
+                forms.widgetFactory(forms.RadioChoice, options=data_strings,
+                    noneOption=differentNone))
+        form.addField('selectOther', forms.String(),
+                forms.widgetFactory(forms.SelectOtherChoice, options=['Mr',
+                    'Mrs']))
+        form.addField('selectOtherRequired', forms.String(required=True),
+                forms.widgetFactory(forms.SelectOtherChoice, options=['Mr',
+                    'Mrs']))
+        form.addField('multiselect', forms.String(),
+                forms.widgetFactory(forms.MultiselectChoice, options=strings))
         form.addAction(self.submitted)
         return form
     
