bcf1ad4bf3d9b3a9a1b2d7ae2ab9fc0c809a5d55
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2006-05-22 17:27:42 +0000

    Bit prettier and remove the incorrect form.data values.

--- branches/fieldgroups/formal/examples/group.py
+++ branches/fieldgroups/formal/examples/group.py
@@ -7,25 +7,22 @@ class GroupFormPage(main.FormExamplePage):
     description = 'Groups of fields on a form'
     
     def form_example(self, ctx):
-        form = formal.Form()
-
-        form.addField('before', formal.String())
 
         group = formal.Group('group')
         group.add(formal.Field('one', formal.String()))
-        group.add(formal.Field('two', formal.String()))
+        group.add(formal.Field('two', formal.Integer()))
         group.add(formal.Field('pass', formal.String(),
             widgetFactory=formal.CheckedPassword))
+
+        form = formal.Form()
+        form.addField('before', formal.String())
         form.add(group)
-
         form.addField('after', formal.String())
-
         form.addAction(self.submitted)
         form.data = {
             'before': 'before',
-            'group': {
-                'one': 'one',
-            },
+            'group.one': 'one',
+            'group.two': 2,
             'after': 'after'
         }
         return form
