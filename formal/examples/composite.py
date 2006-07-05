cfc7da58040a068ed13873ad3fcb31cb5ea9869b
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2006-07-05 09:53:11 +0000

    Move the Component and ComponentWidget classes to correct/better modules.

--- formal/examples/composite.py
+++ formal/examples/composite.py
@@ -1,102 +1,5 @@
-from zope.interface import implements
-from twisted.python.components import registerAdapter
-from nevow import inevow, tags as T
-import formal, formal.iformal, formal.types, formal.widget, formal.util
+import formal
 from formal.examples import main
-
-
-
-#####
-# TODO:
-#
-#  * composition labels, and position (?)
-#  * composition widgets
-#  * composition errors (?)
-#  * composition description (?)
-#  * Validate XHTML
-#
-
-
-
-class Composite(formal.types.Type):
-
-    def __init__(self, composition, *a, **k):
-        super(Composite, self).__init__(*a, **k)
-        self.composition = composition
-
-
-    def validate(self, value):
-
-        # Check we have the correct number of values, otherwise the final value
-        # could be completely wrong because we're relying on zip to build the
-        # dict.
-        if len(value) != len(self.composition):
-            raise ValueError("Incorrect number of values to validate")
-
-        # If nothing has been entered then we'll have a sequence of None
-        # instances, in which case my value if None (not a sequence). If there
-        # is anything other than None in the sequence then pass validation on to
-        # the composite types.
-        if not filter(None, value):
-            value = None
-        else:
-            value = dict([
-                    (name, type.validate(value))
-                    for (name, type), value in zip(self.composition, value)])
-
-        # Allow normal validation to run on the new value
-        return super(Composite, self).validate(value)
-
-
-
-class CompositeWidget(object):
-    implements(formal.iformal.IWidget)
-
-
-    labels = None
-    widgetFactories = None
-
-
-    def __init__(self, composite, labels=None, widgetFactories=None):
-        self.composite = composite
-        if labels is not None:
-            self.labels = labels
-        if widgetFactories is not None:
-            self.widgetFactories = widgetFactories
-
-
-    def render(self, ctx, key, args, errors):
-        for name, type in self.composite.composition:
-            childKey = '.'.join([key, name])
-            yield T.div(class_=name)[
-                T.label(for_=formal.util.render_cssid(childKey))[
-                    formal.util.titleFromName(name)
-                    ],
-                formal.iformal.IWidget(type).render(ctx, childKey, args, errors)
-                ]
-
-
-    def renderImmutable(self, ctx, key, args, errors):
-        for name, type in self.composite.composition:
-            childKey = '.'.join([key, name])
-            yield T.div(class_=name)[
-                T.label[formal.util.titleFromName(name)],
-                formal.iformal.IWidget(type).renderImmutable(ctx, childKey,
-                        args, errors)
-                ]
-
-
-    def processInput(self, ctx, key, args):
-        value = []
-        for name, type in self.composite.composition:
-            childKey = '.'.join([key, name])
-            value.append(formal.iformal.IWidget(type).processInput(ctx,
-                childKey, args))
-        return tuple(value)
-
-
-
-registerAdapter(CompositeWidget, Composite, formal.iformal.IWidget)
 
 
 
@@ -112,19 +15,19 @@ class CompositeFormPage(main.FormExamplePage):
 
         # Add a required name where the family name is required but the first
         # name is optional.
-        form.add(formal.Field('name', Composite([
+        form.add(formal.Field('name', formal.Composite([
             ('family', formal.String(required=True)),
             ('first', formal.String())],
             required=True)))
 
         # Add an optional temperature field where, once entered, both values
         # must be entered.
-        form.add(formal.Field('temperature', Composite([
+        form.add(formal.Field('temperature', formal.Composite([
             ('temperature', formal.Integer(required=True)),
             ('units', formal.String(required=True))])))
 
         # Add a required height field where both values are also required.
-        form.add(formal.Field('height', Composite([
+        form.add(formal.Field('height', formal.Composite([
             ('feet', formal.Integer(required=True)),
             ('inches', formal.Integer(required=True))],
             required=True)))
