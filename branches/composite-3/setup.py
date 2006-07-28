cf0bcc4ab5d2446b786b56f3e1462a0fd4adb72d
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2006-07-28 15:24:04 +0000

    Branch trunk to composite-3 to merge forward.

---
+++ branches/composite-3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='formal',
+    version='0.9.3',
+    description='HTML forms framework for Nevow',
+    author='Matt Goodall',
+    author_email='matt@pollenation.net',
+    packages=find_packages(),
+    package_data={
+        '': ['*.css', '*.html', '*.js'],
+        },
+    zip_safe = True,
+    )
