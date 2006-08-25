491bef40d7028085dc474c33b5a105adc672a4b2
tim <tim@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2006-08-25 14:07:52 +0000

    upped the number in setup.py for textareawithselect and sequencetostringconverter

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='formal',
-    version='0.9.3',
+    version='0.9.4',
     description='HTML forms framework for Nevow',
     author='Matt Goodall',
     author_email='matt@pollenation.net',
