6ce7c2ca2df026e16b63208a0d32438dbb76a77b
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2006-09-11 16:37:42 +0000

    Put back the converter that was accidentally deleted.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='formal',
-    version='0.9.5',
+    version='0.9.6',
     description='HTML forms framework for Nevow',
     author='Matt Goodall',
     author_email='matt@pollenation.net',
