1d68d6e70fe0b9acef5249c385e49eddc6f4a7b2
damian <damian@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2006-10-11 12:38:13 +0000

    Changed version to release the removable FileUploadWidget change.

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='formal',
-    version='0.9.6',
+    version='0.9.7',
     description='HTML forms framework for Nevow',
     author='Matt Goodall',
     author_email='matt@pollenation.net',
