05812389e008658fa1370a7aac2d27d834f30091
tim <tim@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2006-11-30 13:24:55 +0000

    upped the version number and added changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='formal',
-    version='0.9.9',
+    version='0.10.0',
     description='HTML forms framework for Nevow',
     author='Matt Goodall',
     author_email='matt@pollenation.net',
