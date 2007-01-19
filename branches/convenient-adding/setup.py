2b333450805562b2984159cb6b728a9b94709f4a
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2007-01-19 11:50:06 +0000

    Branch to make add things to a form or group more convenient.

---
+++ branches/convenient-adding/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='formal',
+    version='0.10.0',
+    description='HTML forms framework for Nevow',
+    author='Matt Goodall',
+    author_email='matt@pollenation.net',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe = True,
+    )
