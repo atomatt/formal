1381f6f6e59916906d889c0b9240f04f606a5be9
matt <matt@9ce350d5-fc46-0410-bd36-1f1759a9bfbe>
2007-06-06 10:22:46 +0000

    Register the TextAreaList as the default widget for Sequence types for "out of the box" ease of use.
    I imagine the TextAreaList is not the best widget most of the time but it's
    nice when the form just works for quick protyping, but can still be customised
    to use a better widget.

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='formal',
-    version='0.13',
+    version='0.13.1',
     description='HTML forms framework for Nevow',
     author='Matt Goodall',
     author_email='matt@pollenation.net',
